972b4a752ad577aa1822c0ac5b0172299ab56f25
Joern <924901972@qq.com>
2021-03-03 21:08:26 +0800

    feat:新增koa session与redis连接

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const json = require('koa-json');
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser'); // 处理postData
 const logger = require('koa-logger');
+
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
+
 // 路由处理引入
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -17,23 +21,38 @@ onerror(app);
 // middlewares
 // app.use和express基本一样
 app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }));
 app.use(json()); // 处理postData中上传数据 - 原理和之前都是一样的
 app.use(logger()); // 日志
 app.use(require('koa-static')(__dirname + '/public'));
 
 app.use(views(__dirname + '/views', {
-  extension: 'pug'
+    extension: 'pug'
 }));
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date(); //获取当前时间
-  await next(); // 做其他处理
-  const ms = new Date() - start; //做完之后再获取时间
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`); // 看服务请求耗时~
+    const start = new Date(); //获取当前时间
+    await next(); // 做其他处理
+    const ms = new Date() - start; //做完之后再获取时间
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`); // 看服务请求耗时~
 });
+
+// session和redis连接一定要在路由之前写
+app.keys = ['WJiol#23123']; // 同express配置secret功能一致
+app.use(session({
+    // 配置 cookie
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000
+    },
+    // 配置 redis
+    store: redisStore({
+        all: '127.0.0.1:6379' // 先写死本地redis服务端口 ,后续需要根据环境配置
+    })
+}));
 
 // routes
 app.use(index.routes(), index.allowedMethods()); // 针对路由如果访问有问题会有一个兜底
@@ -43,7 +62,7 @@ app.use(user.routes(), user.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    console.error('server error', err, ctx)
 });
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -12,5 +12,18 @@ router.post('/login', async function (ctx, next) {
     }
 });
 
+router.get('/session-test', async function (ctx, next) {
+    // 之前注册的session中间件才会有内容
+    if (ctx.session.viewCount == null) {
+        ctx.session.viewCount = 0;
+    }
+    ctx.session.viewCount++;
+
+    ctx.body = {
+        errno: 0,
+        viewCount: ctx.session.viewCount
+    }
+});
+
 // 定义了新的路由记得去app.js引用一下
 module.exports = router;
