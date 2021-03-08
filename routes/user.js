eb78ff00d1cd9df2250b63a3e775dd5bfec79843
Joern <924901972@qq.com>
2021-03-08 21:52:47 +0800

    feat:开发user.js接口路由

--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,36 @@
 const router = require('koa-router')();
+const {login} = require('../controller/user');
+const {SuccessModel, ErrorModel} = require('../model/resModel');
 
 router.prefix('/api/user');
 
 router.post('/login', async function (ctx, next) {
-    // 直接body没办法获取，被占用了
     const {username, password} = ctx.request.body;
-    ctx.body = {
-        errno: 0,
-        username,
-        password
+    const data = await login(username, password);
+    if (data.username) {
+        // 设置 session
+        ctx.session.username = data.username;
+        ctx.session.realname = data.realname;
+        ctx.body = new SuccessModel(); // 使用ctx.body返回
+        return;
     }
+    ctx.body = new ErrorModel('登录失败');
+    // const result = login(username, password);
+    // return result.then(data => {
+    //     if (data.username) {
+    //         // 设置 session
+    //         req.session.username = data.username;
+    //         req.session.realname = data.realname;
+    //         // 同步到 redis - 这里使用express-session话会自动同步到redis去，不需要再写
+    //         // set(req.sessionId, req.session);
+    //
+    //         res.json(new SuccessModel());
+    //         return;
+    //     }
+    //     res.json(new ErrorModel('登录失败'));
+    // })
 });
 
-router.get('/session-test', async function (ctx, next) {
-    // 之前注册的session中间件才会有内容
-    if (ctx.session.viewCount == null) {
-        ctx.session.viewCount = 0;
-    }
-    ctx.session.viewCount++;
-
-    ctx.body = {
-        errno: 0,
-        viewCount: ctx.session.viewCount
-    }
-});
 
 // 定义了新的路由记得去app.js引用一下
 module.exports = router;
